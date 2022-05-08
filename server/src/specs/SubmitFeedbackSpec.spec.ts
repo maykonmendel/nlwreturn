137d7042676a2fbd85e68e18e69fd228cce8f463
Maykon Mendel <maykonmendel.dev@gmail.com>
2022-05-08 15:26:54 -0300

    Add tests specs

--- server/src/specs/SubmitFeedbackSpec.spec.ts
+++ server/src/specs/SubmitFeedbackSpec.spec.ts
@@ -1,10 +1,16 @@
 import { SubmitFeedbackService } from "../services/SubmitFeedbackService"
 
 describe("Submit Feedback", () => {
-    it("should be able to submit a feedback", () => {
+    it("should be able to submit a feedback", async () => {
         const submitFeedback = new SubmitFeedbackService(
             { create: async () => {} },
             { sendMail: async () => {}},
         );
+
+        await expect(submitFeedback.execute({
+            type: "BUG",
+            comment: "Example comment",
+            screenshot: "data:image/png;base64",
+        })).resolves.not.toThrow;
     })
 })
